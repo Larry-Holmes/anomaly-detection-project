15d24dea55ce8b892438674e4219a5045bf19c6c
Larry Holmes <larry.b.holmes.2@gmail.com>
2022-10-07 12:30:34 -0500

    finalizing deliverables

--- wrangle.py
+++ wrangle.py
@@ -43,10 +43,44 @@ def pull_data():
     return df
 
 
-def prep(df):
+def prep():
     
+    #acquire data
+    df = pull_data()
+    
+    # removes null from dataframe (no path recorded)
     df = df.drop(df.index[478630])
-                 
+    
+    # removes unnecessary column that mimics index
+    df = df.iloc[:,1:]
+    
+    # removes erroneous unknown program type
+    df = df[df.program_id != 4]
+    
+    # convert id #'s into program names 
+    df['program_id'] = np.where(df.program_id == 3, 'data_science', 'web_development')
+    
+    # renaming column
+    df = df.rename(columns={'program_id': 'program', 'cohort_id': 'cohort', 'user_id': 'user'})
+    
+    df['date'] = df['date'] + ' ' + df['time']
+    
+    df = df.drop(columns = ['time'])
+    
+    df = lesson_sep(df)
+    
+    df['unit'] = np.where(df.unit == '', 'table_of_contents', df.unit)
+    
+    df = df.fillna('')
+    
+    df = df[df.unit != 'table_of_contents']
+    
+    df = df[df.name != 'Staff']
+    
+    df = df[(df.lesson != '') & (df.lesson != 'introduction')]
+    
+    df = df[(df.lesson != 'overview') & (df.lesson != 'search_index.json')]
+
     return df
     
 
@@ -60,19 +94,78 @@ def convert_date(df, col):
     
     return df
 
-def lesson_sep(df, col):
+def lesson_sep(df):
     
     '''
     
     '''
     
-    lessons = []
-    non_lessons = []
-    for i in df[col]:
-        if df[col][i].find('/') < 0:
-            non_lessons.append(df[col][i])
-        else:
-            lessons.append(df[col][i])
+    unit_lesson = df.path.str.split('/',1,expand=True)
+    
+    unit_lesson.columns = ['unit','lesson']
+    
+    df = pd.concat([df, unit_lesson], axis=1)
+    
+    df = df.drop(columns= ['path'])
   
-    return lessons, non_lessons
+    return df
+
+
+def get_lower_and_upper_bounds(col, mult=1.5):
+    '''
+    positional arguments:
+    col: a pandas Series
+    keyword arguments:
+    mult: default 1.5, the magnutude specified for the IQR rule
     
+    returns:
+    lower_bound, upper_bound: two float values representing the fence values requested
+    '''
+    q1 = col.quantile(0.25)
+    q3 = col.quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - mult * iqr
+    upper_bound = q3 + mult * iqr
+    return lower_bound,upper_bound
+
+def max_df(df):
+    
+    df = df[(df.lesson != '') & (df.lesson != 'introduction')]
+    cohort_lessons = df.groupby('name')['lesson'].value_counts()
+
+    cohort_lessons = pd.DataFrame(cohort_lessons)
+    cohort_lessons = cohort_lessons.rename(columns = {'lesson' : 'number_of_hits'})
+    
+    cohort_lessons = cohort_lessons.reset_index()
+    
+    max_lessons = cohort_lessons.groupby('name').max()
+    
+    cohort_lessons = cohort_lessons.set_index('name')
+    
+    final = pd.DataFrame()
+    for i in max_lessons.index:
+        new = cohort_lessons.loc[i][cohort_lessons.loc[i]['number_of_hits'] == max_lessons.loc[i]['number_of_hits']]
+        final = pd.concat([final, new])
+    
+    return final
+
+def min_df(df):
+    
+    df = df[df.lesson != '']
+    cohort_lessons = df.groupby('name')['lesson'].value_counts()
+
+    cohort_lessons = pd.DataFrame(cohort_lessons)
+    cohort_lessons = cohort_lessons.rename(columns = {'lesson' : 'number_of_hits'})
+    
+    cohort_lessons = cohort_lessons.reset_index()
+    
+    min_lessons = cohort_lessons.groupby('name').min()
+    
+    cohort_lessons = cohort_lessons.set_index('name')
+    
+    final = pd.DataFrame()
+    for i in min_lessons.index:
+        new = cohort_lessons.loc[i][cohort_lessons.loc[i]['number_of_hits'] == min_lessons.loc[i]['number_of_hits']]
+        final = pd.concat([final, new])
+    
+    return final
